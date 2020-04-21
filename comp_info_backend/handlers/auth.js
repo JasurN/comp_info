3ab2ffe50726484ca6e5ee48c9ec85d7fdea781a
jasurn <97jasur@gmail.com>
2020-04-21 15:18:38 +0500

    backend: 1. added sign in and sign up routes and handlers for these routes

---
+++ comp_info_backend/handlers/auth.js
@@ -0,0 +1,59 @@
+const db = require("../models/index");
+const jwt = require("jsonwebtoken");
+
+exports.signin = async function (request, response, next) {
+    try {
+        let user = await db.User.findOne({
+            username: request.body.username
+        });
+
+        let {id, username} = user;
+        let isMatch = await user.comparePassword(request.body.password);
+        if (isMatch) {
+            let token = jwt.sign(
+                {
+                    id,
+                    username,
+                },
+                process.env.SECRET_KEY
+            );
+            return response.status(200).json({
+                id,
+                username,
+                token
+            });
+        } else {
+            return next({
+                status: 400,
+                message: "Invalid Username/Password"
+            });
+        }
+    } catch (error) {
+        return next({status: 400, message: `${error.message} catch block`});
+    }
+};
+
+exports.signup = async function (request, response, next) {
+    try {
+        let user = await db.User.create(request.body);
+        let {id, username} = user;
+        let token = jwt.sign({
+                id,
+                username,
+            },
+            process.env.SECRET_KEY);
+        return response.status(200).json({
+            id,
+            username,
+            token
+        })
+    } catch (error) {
+        if (error.code === 11000) {
+            error.message = "Sorry that username and/or email is taken";
+        }
+        return next({
+            status: 400,
+            message: error.message
+        });
+    }
+};
--- comp_info_backend/index.js
+++ comp_info_backend/index.js
@@ -2,13 +2,22 @@ require("dotenv").config(); // loads all env to process.env
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const authRoutes = require("./router/auth");
 const errorHandlers = require("./handlers/error");
 
 const PORT = 8081;
 
+// enable cors
 app.use(cors());
+app.use(bodyParser.json());
 
+
+// routes for authorization
+app.use("/api/auth", authRoutes);
+
+// handle invalid routes and return 404
 app.use(function (req, res, next) {
     let error = new Error("Not found");
     error.status = 404;
