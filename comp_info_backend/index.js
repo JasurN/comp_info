8f473125e097f2f6886c38acf81d4a24e7c4beae
jasurn <97jasur@gmail.com>
2020-04-21 14:42:17 +0500

    1. backend - added error handlers and 404 error handler 2. frontend - created initial commit for frontend

--- comp_info_backend/index.js
+++ comp_info_backend/index.js
@@ -1,10 +1,22 @@
+require("dotenv").config(); // loads all env to process.env
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const errorHandlers = require("./handlers/error");
+
 const PORT = 8081;
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+    let error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use(errorHandlers);
+
 
 app.listen(PORT, function () {
     console.log(`Server is starting on port ${PORT}`);
